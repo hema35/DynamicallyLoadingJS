2ce9418b6ef3abf672626e993ea222efee448a93
hema35 <cheershema@gmail.com>
2021-07-31 19:35:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ps4bfb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ps4bfb)
---
+++ index.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+function makeMenu() {
+  var h2s = document.getElementsByTagName('h2');
+  var menu = document.createElement('div');
+  var menuUl = document.createElement('ul');
+  menu.appendChild(menuUl);
+  for (var i = 0; i < h2s.length; i++) {
+    var itemText = h2s[i].childNodes[0].nodeValue;
+    var menuLi = document.createElement('li');
+    menuUl.appendChild(menuLi);
+    var menuLiA = document.createElement('a');
+    menuLiA = menuLi.appendChild(menuLiA);
+    menuLiA.setAttribute('href', '#item' + i);
+    var menuText = document.createTextNode(itemText);
+    menuLiA.appendChild(menuText);
+    var anc = document.createElement('a');
+    anc.setAttribute('name', 'item' + i);
+    document.body.insertBefore(anc, h2s[i]);
+  }
+  document.body.insertBefore(menu, document.body.firstChild);
+}
+
+window.onload = function(){
+  if (window.jQuery) {  
+    // jQuery is loaded  
+    alert("Yeah!");
+} else {
+    // jQuery is not loaded
+    alert("Doesn't Work");
+}
+}
